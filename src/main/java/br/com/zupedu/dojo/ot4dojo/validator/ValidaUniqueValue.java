075d9490cdc9a416104c417eba64ed5782e27025
Henio Junior <henioajunior@gmail.com>
2021-07-27 12:16:34 -0300

    piloto Henio copiloto Nicole

--- src/main/java/br/com/zupedu/dojo/ot4dojo/validator/ValidaUniqueValue.java
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/validator/ValidaUniqueValue.java
@@ -1,7 +1,21 @@
 package br.com.zupedu.dojo.ot4dojo.validator;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
 
-@Constraint(validatedBy = {Uniqu})
+
+@Constraint(validatedBy ={ UniqueValueValidator.class})
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface UniqueValue {
+    String message() default "Erro de validação";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+      Class<?> domainClass();
+
+
 }
